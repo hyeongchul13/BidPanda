f10d6d47462c20c88a17773e82334765b121498d
hyeongchul13 <jc_22@naver.com>
2023-10-24 22:12:25 +0900

    deploy.sh update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,15 +9,9 @@ EXIST_BLUE=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.bl
 BLUE_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml ps | grep Up)
 GREEN_HEALTH=$(sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml ps | grep Up)
 
-# 배포 시작한 날짜와 시간을 기록
-echo "배포 시작일자 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-
 # green이 실행중이면 blue up
 # EXIST_BLUE 변수가 비어있는지 확인
-if [ "$EXIST_BLUE" ]; then
-
-  # 로그 파일(/home/ubuntu/deploy.log)에 "blue up - blue 배포 : port:8081"이라는 내용을 추가
-  echo "blue 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
+if [ -z "$EXIST_BLUE" ]; then
 
     # docker-compose.blue.yml 파일을 사용하여 docker-blue 프로젝트의 컨테이너를 빌드하고 실행
     sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml up -d --build
@@ -26,39 +20,28 @@ if [ "$EXIST_BLUE" ]; then
   sleep 30
 
  # blue가 현재 실행중이지 않다면 -> 런타임 에러 또는 다른 이유로 배포가 되지 못한 상태
-  if [ "$BLUE_HEALTH" ]; then
+  if [ -z "$BLUE_HEALTH" ]; then
     # slack으로 알람을 보낼 수 있는 스크립트를 실행한다.
     sudo ./slack_blue.sh
   # blue가 현재 실행되고 있는 경우에만 green을 종료
   else
-
-    # /home/ubuntu/deploy.log: 로그 파일에 "green 중단 시작"이라는 내용을 추가
-    echo "green 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
-
     # docker-compose.green.yml 파일을 사용하여 docker-green 프로젝트의 컨테이너를 중지
     sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml down
 
      # 사용하지 않는 이미지 삭제
     sudo docker image prune -af
-
-    echo "green 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
   fi
 
 # blue가 실행중이면 green up
 else
-  echo "green 배포 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
   sudo docker-compose -p ${DOCKER_APP_NAME}-green -f docker-compose.green.yml up -d --build
 
   sleep 30
 
-  if [ "$GREEN_HEALTH" ]; then
+  if [ -z "$GREEN_HEALTH" ]; then
       sudo ./slack_green.sh
   else
-
-    echo "blue 중단 시작 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
     sudo docker-compose -p ${DOCKER_APP_NAME}-blue -f docker-compose.blue.yml down
     sudo docker image prune -af
-
-    echo "blue 중단 완료 : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)" >> /home/ubuntu/deploy.log
   fi
 fi
